7fc42578b3bfc06e501d25cda87dfdd6591f1a92
sahithip04 <palparthisahithireddy@gmail.com>
2025-12-16 20:35:05 +0530

    userdata app is done

---
+++ useEffect_Q1/user-data-app/src/UserData.jsx
@@ -0,0 +1,29 @@
+import { useEffect, useState } from "react";
+
+function UserData() {
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/users/1")
+      .then((response) => response.json())
+      .then((data) => {
+        setUser(data);
+        setLoading(false);
+      });
+  }, []);
+
+  if (loading) {
+    return <p>Loading...</p>;
+  }
+
+  return (
+    <div>
+      <p><strong>Name:</strong> {user.name}</p>
+      <p><strong>Email:</strong> {user.email}</p>
+      <p><strong>Phone:</strong> {user.phone}</p>
+    </div>
+  );
+}
+
+export default UserData;
